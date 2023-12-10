73926a24a18446d9e8b1a25284c5342c0493ce63
kyoda <kyodanh@gmail.com>
2023-12-10 15:47:01 +0700

    build jenkin

--- src/test/java/Runner/TestRunner_Login.java
+++ src/test/java/Runner/TestRunner_Login.java
@@ -4,6 +4,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+
 @RunWith(Cucumber.class)
 @CucumberOptions(features = ".\\src\\test\\java\\Feature\\A_Login.feature",
         glue = {"Steps"},
