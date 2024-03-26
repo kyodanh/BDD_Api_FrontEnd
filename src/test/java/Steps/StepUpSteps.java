e56452d9fd10889ed3403fb149b31273a43a8181
kyoda <kyodanh@gmail.com>
2024-03-26 17:06:25 +0700

    update tự động driver

--- src/test/java/Steps/StepUpSteps.java
+++ src/test/java/Steps/StepUpSteps.java
@@ -6,6 +6,7 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -24,7 +25,7 @@ public class StepUpSteps {
 
     @Before
     public void browers_setup(){
-        System.setProperty("webdriver.chrome.driver", ".\\BDD_Api_FrontEnd\\msedgedriver.exe");
+        WebDriverManager.edgedriver().setup();
         driver = new EdgeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
